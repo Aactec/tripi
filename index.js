1a16ab91904d78ca49c0bb5d3a3c94fff22f9571
Aactec <aacrajitech2024@gmail.com>
2023-12-08 14:20:43 +0100

    Newly Up to date

---
+++ index.js
@@ -0,0 +1,43 @@
+const navLinks = document.getElementById("navLink");
+function showMenu() {
+    navLinks.style.right = "0%";
+}
+
+let scrollBody = document.querySelector(".revgrid");
+let rightArrow = document.getElementById("rightArrow");
+let leftArrow = document.getElementById("leftArrow");
+
+rightArrow.addEventListener("click", () => {
+    scrollBody.style.scrollBehavior = "smooth";
+    scrollBody.scrollLeft += 314;
+});
+leftArrow.addEventListener("click", () => {
+    scrollBody.style.scrollBehavior = "smooth";
+    scrollBody.scrollLeft -= 314;
+});
+
+let destBody = document.querySelector(".destgrid");
+let rightDest = document.getElementById("rightDest");
+let leftDest = document.getElementById("leftDest");
+
+rightDest.addEventListener("click", () => {
+    destBody.style.scrollBehavior = "smooth";
+    destBody.scrollLeft += 340;
+});
+leftDest.addEventListener("click", () => {
+    destBody.style.scrollBehavior = "smooth";
+    destBody.scrollLeft -= 340;
+});
+
+let carouselBody = document.querySelector(".imagecarousel");
+let rightCarousel = document.getElementById("arright");
+let leftCarousel = document.getElementById("arleft");
+
+rightCarousel.addEventListener("click", () => {
+    carouselBody.style.scrollBehavior = "smooth";
+    carouselBody.scrollLeft += 264;
+});
+leftCarousel.addEventListener("click", () => {
+    carouselBody.style.scrollBehavior = "smooth";
+    carouselBody.scrollLeft -= 264;
+});
